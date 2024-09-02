3a7dc183a67d783d1c9a54dcac19dc8230f6c709
Flanker745 <pradeepnain745@gmail.com>
2024-09-02 09:46:03 +0530

    commit after forget pass done

--- client/src/components/UserContext.jsx
+++ client/src/components/UserContext.jsx
@@ -6,7 +6,8 @@ export const UserContext = createContext();
 export const UserProvider = ({ children }) => {
   const [user, setUser] = useState(null);
   const [sidebarToggle, setSidebarToggle] = useState(false);
-  const api = "https://serverapi.studywisdom.com";
+  // const api = "https://serverapi.studywisdom.com";
+  const api = "https://serverapi.studywisdom.com"
   const [existUser, setExistUser] = useState(null);
   const [exitUserId, setExitUserId] = useState(null);
   const [exitUserDetails, setExitUserDetails] = useState([]);
